e7509f5b73c418edf62f2f7b935aa7ed9ca0cb9b
Thomas Maschler <tmaschler@wri.org>
2019-04-09 10:03:00 -0400

    update bootstrap
    Signed-off-by: Thomas Maschler <tmaschler@wri.org>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
-add-apt-repository ppa:ubuntugis/ppa && apt-get update && apt-get -y upgrade
-apt-get --yes install python3.6-dev gdal-bin libgdal-dev python3-pip ca-certificates git
+#! /bin/sh
+
+add-apt-repository -y ppa:ubuntugis/ppa && apt-get -y update && apt-get -y upgrade
+apt-get -y install python3.6-dev gdal-bin libgdal-dev python3-pip ca-certificates git
 mkdir -p /etc/pki/tls/certs
 cp /etc/ssl/certs/ca-certificates.crt /etc/pki/tls/certs/ca-bundle.crt
-cd ~
+cd /home/ubuntu
 git clone https://github.com/wri/compute_histogram.git
 cd compute_histogram
 pip3 install -e .
